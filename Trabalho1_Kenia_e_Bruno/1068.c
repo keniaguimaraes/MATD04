42ae5410e5ca3ef4984e15dc3e4243bae7e4cfea
Kênia Guimarães <keniiarruda@gmail.com>
2018-10-24 11:33:21 -0300

    Adiciona o exercicio 1068 do URI.

---
+++ Trabalho1_Kenia_e_Bruno/1068.c
@@ -0,0 +1,80 @@
+//Problema 1068 - Balanço de parênteses.
+//Alunos:Bruno Vasconcelos e Kenia Guimarães.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+struct expressao 	{
+  char par;
+  struct expressao *prox;
+};
+typedef struct expressao TExpressao;
+
+
+struct pilha { 	
+    TExpressao *topo;
+    int qtd;
+};
+typedef struct pilha ExpPilha;
+
+
+//Programa Principal
+int main(){
+
+	char expressao[3000] = ""; //tamnho da expressao
+	int c;
+	int sair;//flag saida
+
+	ExpPilha	pilhaExp;
+	TExpressao * aux;
+	
+	while (scanf("%s", expressao) != EOF) //enquanto expressao for <> nulo
+	{
+		pilhaExp.topo = NULL;
+		pilhaExp.qtd = c;
+		sair = 0;  //
+
+		do
+		{
+			if(expressao[c] == '(') //achou o (
+			{
+				aux = (TExpressao *) malloc(sizeof(TExpressao));
+
+				if(aux == NULL){
+					break;
+				}
+					
+				aux->par = expressao[c]; //adiciona o (
+				aux->prox = pilhaExp.topo;
+
+				pilhaExp.topo = aux;
+				pilhaExp.qtd++;
+			}
+
+			if(exp[c] == ')') //achou a )
+
+			{
+				if(pilhaExp.topo == NULL)
+				{
+					sair = 1;
+					break;
+				}
+
+				else
+				{
+					aux = pilhaExp.topo;
+					pilhaExp.topo = aux->prox;
+					free(aux);
+					pilhaExp.qtd--;
+				}
+			}
+			c++;
+		} while (c < strlen(expressao));
+
+	
+	}
+
+	return 0;
+}
