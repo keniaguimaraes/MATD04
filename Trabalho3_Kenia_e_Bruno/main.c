ea56f7d55045ac1de80db09cfb60229fd0c8a6cc
Kenia Guimaraes <keniiarruda@gmail.com>
2018-12-06 19:55:17 -0300

    Adiciona metodo de ordenacao por bubblesort.

--- Trabalho3_Kenia_e_Bruno/main.c
+++ Trabalho3_Kenia_e_Bruno/main.c
@@ -1,57 +1,113 @@
 //Trabalho Estrutura de Dados .
-//Alunos:Bruno Vasconcelos e Kenia Guimarães.
+//Alunos:Bruno Vasconcelos e Kenia Guimaraes.
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 #include <time.h>
-#include <conio.h>
 
-//Declaração de variaveis e estruturas.
+//Declaraco de variaveis e estruturas.
 #define tamanho  100
+#define valrand  10000
 
 int  vetor[tamanho];
+int c;
+  char buffer[30];
+  struct timeval tv;
 
-//Declaração de procedimentos.
+  time_t curtime;
+
+
+
+//Declaracao de procedimentos.
 
 //Menu
 void menu(){
-  system("cls");	
-  printf("1. Geracao do conjunto de dados\n");	
-  printf("2. Pesquisa sequencial\n");	
-  printf("3. Ordenacao\n");	
+  system("clear");	
+  printf(" 1. Geracao do conjunto de dados\n");	
+  printf(" 2. Pesquisa sequencial\n");	
+  printf(" 3. Ordenacao\n");	
   printf("   a. Insercao direta\n");	
   printf("   b. Troca\n");	
   printf("   c. Selecao\n");	
-  printf("4. Pesquisa binaria\n");	
-  printf("5. Sair do programa\n");	   
+  printf(" 4. Pesquisa binaria\n");	
+  printf(" 5. Sair do programa\n");	   
 }
 
 
 void geraConjunto(){
-int i;
-for (i=0; i<tamanho; i++)
-   vetor[i]	= rand() % tamanho;
+
+ srand(tamanho);
+ for (int i=0; i<tamanho; i++)
+   vetor[i]	= rand() % valrand;
 }
+
 void pesquisaSeq(){
   int i,numero, achou;
   achou = 0;
   printf("Informe um numero: ");
   scanf("%d",&numero);
-  for (i=0; i<tamanho; i++) {
-   if (vetor[i]==numero){
-    printf("Valor encontrado na posição %d", i);
+  
+  
+   
+ gettimeofday(&tv, NULL); 
+  curtime=tv.tv_sec;
+
+  
+  for (i=0; i< tamanho; i++) {
+   if (vetor[i]== numero){
+    printf("Valor encontrado na posicao %d\n ", i);
     achou=1;
-    getch;
    }
- }
- if (achou != 0)
-  printf("Valor não encontrado!");
+  }
+ 
+ if (achou !=1){
+  printf("Valor nao encontrado!\n");
+ } 
+  
+  strftime(buffer,30,"%m-%d-%Y  %T.",localtime(&curtime));
+  //printf("%s%ld\n",buffer,tv.tv_usec);
+
+  
+  scanf("%d", &c);
 }
-//Progrma principal
+
+
+void bublleshort(){
+  int x, y, aux;
+      
+  for( x = 0; x < tamanho; x++ )
+  {
+    for( y = x + 1; y < tamanho; y++ ) // sempre 1 elemento Ã  frente
+    {
+      // se o (x > (x+1)) entÃ£o o x passa pra frente (ordem crescente)
+      if ( vetor[x] > vetor[y] )
+      {
+         aux = vetor[x];
+         vetor[x] = vetor[y];
+         vetor[y] = aux;
+      }
+    }
+  } 
+  
+  
+  //mostra vetor ordenado
+  if (tamanho < 10000){
+  for( x = 0; x < tamanho; x++ ){
+    printf("%d | ",vetor[x]); 
+  }
+ }
+  printf("\nVetor Ordenado!\n");
+  scanf("%d", &c);
+  
+}
+
+//Progama principal
 int main() {
   int opcao = -1;
-  int n;
-
+  char op;
+  
+ 
   do {
        menu();
        printf("Informe a Opcao: ");
@@ -61,11 +117,20 @@ int main() {
 		 case 1: geraConjunto();
          		  break;
 				 
-         case 2: 
+         case 2: pesquisaSeq();
    		          break;
 				 
-         case 3: 
-		         break;
+         case 3: { 
+			       //consertar essa chamada de menu
+			       printf("\n Informe a letra: ");
+                   scanf("%c", &op);
+                   if (op == 'a'){
+					printf("Nao implementado!\n");
+					} else if (op == 'a'){  
+					   bublleshort();break;
+					 } else { printf("\n Nao implementado\n"); }
+				   			 
+		         }break;
 				
          case 4:  break;
 		        
@@ -75,4 +140,5 @@ int main() {
   } while (opcao != 0);
 
 }
+
 
